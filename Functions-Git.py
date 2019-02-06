7f192012fc2e28bc589697ae6b29fd5081763b75
Logan Pennock <02logan.pennock@gulllakecs.org>
2019-02-06 13:04:32 -0500

    Added a Greetings Function

---
+++ Functions-Git.py
@@ -0,0 +1,11 @@
+# 4.13.3: Greetings
+# Logan Pennock
+# 2.5.19
+
+name = input("what is your name: ")
+
+def greeting():
+    print("Hi there " + name + "!")
+    print("Nice to meet you")
+
+greeting()
