15e87ba7ccef81abeb28ae576b53e80da364d0ef
Logan Pennock <02logan.pennock@gulllakecs.org>
2019-02-28 12:07:43 -0500

    Add Default Parameter Values

--- Functions-Git.py
+++ Functions-Git.py
@@ -43,3 +43,15 @@ def name_and_age(name, age):
 
 name_and_age('Mike', 33)
 name_and_age('Zane', 18)
+
+
+# 4.14.5: Default Parameter Values
+# Logan Pennock
+# 2.19.19
+
+def print_two_numbers(x, y = 20):
+    print('First number:', x)
+    print('Second number: ' + str(y))
+
+print_two_numbers(34, 45)
+print_two_numbers(78)
